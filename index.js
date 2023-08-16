8a55af419291dac01f29361c99cd3fd251a20896
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-08-16 23:31:15 +0300

    phase-1-class-extension-lab-updated

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-// Your code here
-class Formatter {
-  static capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
   }
 
-  static sanitize(str) {
-    return str.replace(/[^a-zA-Z0-9 '-]/g, "");
+  get countSides() {
+    return this.sides.length;
   }
 
-  static titleize(str) {
-    const exceptions = [
-      "the",
-      "a",
-      "an",
-      "but",
-      "of",
-      "and",
-      "for",
-      "at",
-      "by",
-      "from",
-    ];
-    const words = str.split(" ");
-    const titleizedWords = words.map((word, index) => {
-      if (index === 0 || !exceptions.includes(word)) {
-        return this.capitalize(word);
-      } else {
-        return word;
-      }
-    });
-    return titleizedWords.join(" ");
+  get perimeter() {
+    return this.sides.reduce((sum, side) => sum + side, 0);
   }
 }
 
-// Testing the static methods
-console.log(Formatter.capitalize("hello")); // 'Hello'
-console.log(Formatter.sanitize("Hello, I'm a string!")); // "Hello I'm a string"
-console.log(Formatter.titleize("the quick brown fox jumps over the lazy dog")); // 'The Quick Brown Fox Jumps over the Lazy Dog'
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides !== 3) return false;
+
+    const [side1, side2, side3] = this.sides;
+    return (
+      side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+    );
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.countSides !== 4) return false;
+
+    return this.sides.every((side) => side === this.sides[0]);
+  }
+
+  get area() {
+    if (!this.isValid) return undefined;
+    return this.sides[0] ** 2;
+  }
+}
+
+// Testing the classes
+const triangle = new Triangle([3, 4, 5]);
+console.log("Triangle Perimeter:", triangle.perimeter); // Should be 12
+console.log("Triangle isValid:", triangle.isValid); // Should be true
+
+const square = new Square([4, 4, 4, 4]);
+console.log("Square Perimeter:", square.perimeter); // Should be 16
+console.log("Square isValid:", square.isValid); // Should be true
+console.log("Square Area:", square.area); // Should be 16
+
+const invalidTriangle = new Triangle([1, 2, 3]);
+console.log("Invalid Triangle isValid:", invalidTriangle.isValid); // Should be false
+
+const invalidSquare = new Square([3, 3, 3, 3]);
+console.log("Invalid Square isValid:", invalidSquare.isValid); // Should be false
+console.log("Invalid Square Area:", invalidSquare.area); // Should be undefined
